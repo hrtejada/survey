9938329cbe85509958acd4f8a6d37c6eedf631a4
hrtejada <hr.tejada121@gmail.com>
2016-03-17 10:22:48 -0600

    configured routes to login in and surveys.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
   resources :single_surveys
+  get 'surveys', to: 'single_surveys#index'
   root to: 'visitors#index'
   devise_for :users
+  devise_scope :user do
+    get "/login" => "devise/sessions#new"
+  end
+  devise_scope :user do
+    delete "/logout" => "devise/sessions#destroy"
+  end
   resources :users
 end
